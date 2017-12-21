671d55f199fa89b46c229c8c85b66acb1c21cc15
Matt Hemsteger <bitbucket@sharecentric.com>
2017-12-21 14:44:14 -0800

    adding unit test coverage

--- src/api/worldgen/worldExports/worldSitesAndPops.js
+++ src/api/worldgen/worldExports/worldSitesAndPops.js
@@ -2,6 +2,7 @@ import path from 'path';
 import R from 'ramda';
 import iconv from 'iconv-lite';
 import _debug from 'debug';
+import {of as futureOf, reject as rejectedFutureOf} from 'fluture';
 import {fs} from './../../../utility/fs';
 import createRegionWorldSitesPopsParser from './../../../dsl/parsers/regionWorldSitesPops';
 
@@ -9,12 +10,15 @@ const debug = _debug('df:api:worldgen:worldExports:worldSitesAndPops');
 
 export default function parseWorldSitesAndPops({filePath, creatures}) {
 	return R.compose(
-		R.map(R.compose(
-			R.prop('value'),
-			R.when(R.complement(R.prop('status')), ({index, expected}) => {
-				debug('parse errors: %o', {index, expected});
-				throw new Error(`Error while parsing ${filePath}.`);
-			}),
+		R.chain(R.compose(
+			R.ifElse(
+				R.complement(R.prop('status')),
+				R.compose(
+					() => rejectedFutureOf(new Error(`Error while parsing ${filePath}.`)),
+					R.tap(({index, expected}) => debug('parse errors: %o', {index, expected}))
+				),
+				R.compose(futureOf, R.prop('value'))
+			),
 			(contents) => createRegionWorldSitesPopsParser(creatures).file.parse(contents),
 			(fileBuffer) => iconv.decode(fileBuffer, 'cp437')
 		)),
